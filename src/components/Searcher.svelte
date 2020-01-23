10f9f415e220b8e08d0ec90212a19f76ec7fe727
John Preston <wcerfgba@riseup.net>
2020-01-23 21:24:41 +0000

    Add some styling

--- src/components/Searcher.svelte
+++ src/components/Searcher.svelte
@@ -8,16 +8,35 @@
 </script>
 
 <style>
+.searcher-container {
+  width: 30rem;
+}
+
 a {
   display: block;
+  text-decoration: none;
+  box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.3);
+  padding: 1rem;
+  margin-bottom: 1rem;
+}
+
+
+a p {
+  color: rgba(0, 0, 0, 0.7);
+}
+
+a p:last-child {
+  margin-bottom: 0;
 }
 </style>
 
-<h1>{query}</h1>
+<div class="searcher-container">
+  <h2>{source}</h2>
 
-{#each results as result}
-  <a href={result.link}>
-    <h4>{result.title}</h4>
-    <p>{result.description}</p>
-  </a>
+  {#each results as result}
+    <a href={result.link} target="_blank">
+      <h3>{result.title}</h3>
+      <p>{result.description}</p>
+    </a>
 {/each}
+</div>
